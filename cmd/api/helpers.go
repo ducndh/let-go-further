20b472a6469974df9f2d17a3385e0905663ff24d
ducndh <nguyend@carleton.edu>
2024-04-06 23:55:19 +0700

    customize fully JSON response

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -5,9 +5,11 @@ import (
 	"net/http"
 )
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+type envelope map[string]interface{}
 
-	js, err := json.Marshal(data)
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
+
+	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,6 +7,8 @@ import (
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
+	mux.HandleFunc("/", app.notFoundResponse)
+
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
