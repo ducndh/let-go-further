17eeec15ffb608ba2ac1908f846d11c16b9fca3f
ducndh <nguyend@carleton.edu>
2024-04-14 02:37:59 +0700

    add configuration to the connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -18,7 +18,9 @@ type config struct {
 	port int
 	env  string
 	db   struct {
-		dsn string
+		dsn          string
+		maxOpenConns int
+		maxIdleTime  string
 	}
 }
 
@@ -34,15 +36,17 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://greenlight:password@localhost/greenlight", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
 	flag.Parse()
 
-	dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
+	db, err := openDB(cfg)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
 		os.Exit(1)
 	}
-	defer dbpool.Close()
+	defer db.Close()
+
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
 
 	logger.Printf("database connection pool established")
@@ -63,4 +67,37 @@ func main() {
 	logger.Printf("starting %s server on %s", cfg.env, srv.Addr)
 	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	// Timeout hanging connecting
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Creating the config for the pgxpool
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	config, err := pgxpool.ParseConfig(cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	config.MaxConns = int32(cfg.db.maxOpenConns)
+	config.MaxConnIdleTime = duration
+
+	// Create the pool and ping the context
+	dbpool, err := pgxpool.NewWithConfig(ctx, config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		return nil, err
+	}
+	err = dbpool.Ping(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to ping the database: %v\n", err)
+		return nil, err
+	}
+
+	return dbpool, nil
 }
