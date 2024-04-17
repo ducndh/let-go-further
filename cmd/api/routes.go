1528abf448b11487bc63342cdfd11ecc641670bc
ducndh <nguyend@carleton.edu>
2024-04-18 02:35:06 +0700

    add get all movies endpoint without filtering

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -10,7 +10,10 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/", app.notFoundResponse)
 
 	mux.HandleFunc("GET /v1/healthcheck", app.healthcheckHandler)
+
+	mux.HandleFunc("GET /v1/movies", app.listMoviesHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
+
 	mux.HandleFunc("GET /v1/movies/{id}", app.showMovieHandler)
 	mux.HandleFunc("PATCH /v1/movies/{id}", app.updateMovieHandler)
 	mux.HandleFunc("DELETE /v1/movies/{id}", app.deleteMovieHandler)
