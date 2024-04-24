d696079523288cc0f3aeafc58c8e344d01103268
ducndh <nguyend@carleton.edu>
2024-04-24 23:52:30 +0700

    add token creation

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -124,12 +124,12 @@ func (app *application) background(fn func()) {
 	app.wg.Add(1)
 
 	go func() {
-		// Recover any panic.
 		defer func() {
 			if err := recover(); err != nil {
 				app.logger.PrintError(fmt.Errorf("%s", err), nil)
 			}
 		}()
+
 		fn()
 	}()
 }
